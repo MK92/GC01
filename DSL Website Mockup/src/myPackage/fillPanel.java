91eafe90b999ed1a5e3542b61507bc9695cadf3a
MK92 <konsala.m@gmail.com>
2015-01-07 21:16:54 +0000

    Added listener classes, things work up to a point
    Listeners exist for a label and button. These need to be extended for
    the rest, should be easy to make the dropdown menus etc. now!

--- DSL Website Mockup/src/myPackage/fillPanel.java
+++ DSL Website Mockup/src/myPackage/fillPanel.java
@@ -2,6 +2,8 @@ package myPackage;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
@@ -10,7 +12,7 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class fillPanel implements MouseListener {
+public class fillPanel implements ActionListener {
 	
 	private static final int windowWidth = 400;
 	private static final int windowHeight = 20;
@@ -19,7 +21,8 @@ public class fillPanel implements MouseListener {
 	public static final Color DSL_Dark = Color.decode("#bb0000");
 	
 	public static void fillHeader(JPanel panel) {
-panel.setBackground(DSL_Dark);
+		
+		panel.setBackground(DSL_Dark);
         
 		ImageIcon logoIcon = new ImageIcon("images/logo.png");
 		JLabel logo = new JLabel(logoIcon);
@@ -29,6 +32,9 @@ panel.setBackground(DSL_Dark);
 		JButton loginButton = new JButton(loginIcon);
 		panel.add(loginButton);
 		
+		ActionListener l = null;
+		loginButton.addActionListener(l);
+
 	}
 	
 	public static void fillBody(JPanel panel) {
@@ -38,7 +44,6 @@ panel.setBackground(DSL_Dark);
 	}
 	
 	public static void fillFooter(JPanel panel) {
-		System.out.println("Hello");
 		System.out.println("Hello");
 		
 		panel.setBackground(Color.decode("#bb0000"));
@@ -52,40 +57,29 @@ panel.setBackground(DSL_Dark);
 		JLabel label3 = new JLabel("3");
 		JLabel label4 = new JLabel("4");
 		
-		addListeners.linkListener(label1);
+		//addListeners.linkListener(label1);
+		
+		JButton startButton = new JButton("START");
+		startButton.setSize(50,70);
 
+		startButton.addActionListener(new ButtonListener());
+		panel.add(startButton);
+		
+		
+		label1.addMouseListener(new LabelListener());
+		
 		panel.add(label1);
 		panel.add(label2);
 		panel.add(label3);
 		panel.add(label4);
 		
-	}
-	@Override
-	public void mousePressed(MouseEvent e) {
-	       System.out.println("MousePressed!");
-	    }
-
-	@Override
-	public void mouseClicked(MouseEvent arg0) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
-	public void mouseEntered(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("Action");
 	}
 
-	@Override
-	public void mouseExited(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
 }
